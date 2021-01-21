f05d44b40ea6b26d2f841d4acecb6bff7cea18a7
unknown <rashalismath@gmail.com>
2021-01-21 16:10:05 +0530

    sending activation code for user has been added

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,7 @@ Route::group([
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::post('/activationcode', [AuthController::class, 'generateactivationcode']);
     Route::get('/user-profile', [AuthController::class, 'userProfile']);    
 });
 
