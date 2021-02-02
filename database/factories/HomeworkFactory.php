c68b0046bf8df38b2b9c79afba37c23a05a9f0fb
unknown <rashalismath@gmail.com>
2021-02-02 17:10:53 +0530

    number_of_quoestions added the homework

---
+++ database/factories/HomeworkFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Homework;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+use Carbon\Carbon;
+use Illuminate\Support\Str;
+
+class HomeworkFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Homework::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' =>Str::random(10).$this->faker->randomDigit(),
+            'note' => $this->faker->realText($maxNbChars = 100, $indexSize = 2),
+            'onetime' => $this->faker->boolean($chanceOfGettingTrue = 50),
+            'startDate' => Carbon::now(),
+            'endDate' =>Carbon::now()->addDays(1),
+            'startTime' => Carbon::now(),
+            'endTime' => Carbon::now()->addMinutes(60),
+            'status' => "queued",
+            'number_of_questions' => $this->faker->randomDigit(),
+        ];
+    }
+}
